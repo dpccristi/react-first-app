a1927a1e312748f1f969f7974710dc27830af9f6
Duma Cristi <anonimduma@gmail.com>
2018-09-10 16:30:33 +0300

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Stateless Functinal Component
+const NavBar = ({totalCounters}) => {
+    console.log('Navbar - Rendered');
+
+    return (<nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar{" "}
+            <span className="badge badge-pill badge-secondary">{totalCounters}</span></a>
+    </nav>);
+};
+export default NavBar;
